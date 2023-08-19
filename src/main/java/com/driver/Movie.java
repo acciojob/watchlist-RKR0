3e27bcca692ed8c7ede6622707be7494ad2500de
ramak <ramak@DESKTOP-8SV8CJ8.bbrouter>
2023-08-19 19:39:47 +0530

    added getters and setters

--- src/main/java/com/driver/Movie.java
+++ src/main/java/com/driver/Movie.java
@@ -5,14 +5,51 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+
 public class Movie {
 
 	private String name;
 	private int durationInMinutes;
 	private double imdbRating;
+	
+	
+	
+	
+	
+	
+	
+	public Movie() {}
+	
+	
+	
+	public Movie(String name, int durationInMinutes, double imdbRating) {
+		super();
+		this.name = name;
+		this.durationInMinutes = durationInMinutes;
+		this.imdbRating = imdbRating;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getDurationInMinutes() {
+		return durationInMinutes;
+	}
+	public void setDurationInMinutes(int durationInMinutes) {
+		this.durationInMinutes = durationInMinutes;
+	}
+	public double getImdbRating() {
+		return imdbRating;
+	}
+	public void setImdbRating(double imdbRating) {
+		this.imdbRating = imdbRating;
+	}
+	
+	
+	
 	
 }
